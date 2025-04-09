a94979da16b3fb7d16f05271985deb0b13c8943a
Rosa Kang <therosessom@gmail.com>
2025-04-09 12:16:49 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+
+app.get('/', (req, res)=> {
+    res.send("Welcome to the Subscription API");
+})
+
+app.listen(3000, 'localhost', () => {
+  console.log(`Subscription API is running on: http://localhost:3000`);
+});
+
+export default app;
