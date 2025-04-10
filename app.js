8f15c1a3830754e58890bcbc5052ead01abd327c
Rosa Kang <therosessom@gmail.com>
2025-04-10 16:24:02 -0400

    add arcjet middleware and implement it

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ import authRouter from "./routes/auth.routes.js";
 import connectDB from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware)
 
 // Routes
 app.use('/api/v1/auth', authRouter);
--- config/env.js
+++ config/env.js
@@ -8,7 +8,7 @@ export const {
      DB_URI,
      JWT_SECRET,
      JWT_EXPIRES_IN,
-     ARCJET_API_KEY,
+     ARCJET_KEY,
      ARCJET_ENV 
 } = process.env;
 
